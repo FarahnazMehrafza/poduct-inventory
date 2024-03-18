1fa40bd33d3a9211d27f7aa855a1a3c61bc66ee7
FIRST_NAME LAST_NAME <farahmehr1@gmail.com>
2024-03-18 12:34:06 -0400

    last

--- product backend/src/server.ts
+++ product backend/src/server.ts
@@ -1,32 +1,36 @@
-import express from "express";
-const app = express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./app";
 
-const PORT = 8000;
+// ADD CONNECTION TO MONGODB
+if (
+  process.env.DATABASE == undefined ||
+  process.env.DATABASE_PASSWORD === undefined
+) {
+  throw new Error("DATABASE environment variable(s) not set");
+}
 
-// MIDDLEWARE
-app.use(express.json({ limit: "10kb" }));
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
 
-const todos = [
-  { id: 1, title: "coding" },
-  { id: 2, title: "cleaning" },
-];
-// GET /todos
-app.get("/welcomemsg", (req, res) => {
-  res.status(200).send("Welcome Akeel");
-});
+void mongoose.connect(DB);
 
-// POST Request which console of User added
-app.post("/welcomemsg", (req, res) => {
-  const { username } = req.body;
-  res.status(201).send(`User Created ${username} `);
-});
+mongoose.connection
+  .on("open", () => {
+    console.log("connected to mongo");
+  })
+  .on("close", () => {
+    console.log("disconnected from mongo");
+  })
+  .on("error", (error) => {
+    console.log("connection failed", error);
+  });
 
-app.delete("/welcomemsg/:id", (req, res) => {
-  const { id } = req.params;
-  // remove it from the array
-  res.status(204).send(`User Deleted Successfully`);
-});
-
+// START THE SERVER
+const PORT: string | number = process.env.PORT ?? 8000;
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
